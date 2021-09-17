67a5ab408082d4427c44f31579f126a7e0ca7939
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2021-09-17 14:04:44 +0100

    promise

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -42,86 +42,56 @@ export const fillSongsAction = () => {
         )
       );
 
-      const state = {
-        rockSongs: data[0],
-        popSongs: data[1],
-        hipHopSongs: data[2],
-      };
-      dispatch({
-        type: FILL_SONGS,
-        payload: state,
-      });
-
-      console.log({ state });
+      if (data) {
+        const state = {
+          rockSongs: data[0],
+          popSongs: data[1],
+          hipHopSongs: data[2],
+        };
+        setTimeout(() => {
+          dispatch({
+            type: FILL_SONGS_LOADING,
+            payload: false,
+          });
+        }, 1000);
+        dispatch({
+          type: FILL_SONGS_ERROR,
+          payload: false,
+        });
+        dispatch({
+          type: FILL_SONGS,
+          payload: state,
+        });
+        console.log({ state });
+      } else {
+        console.log("error");
+        setTimeout(() => {
+          dispatch({
+            type: FILL_SONGS_LOADING,
+            payload: false,
+          });
+        }, 1000);
+        setTimeout(() => {
+          dispatch({
+            type: FILL_SONGS_ERROR,
+            payload: true,
+          });
+        }, 1000);
+      }
     } catch (error) {
       console.log(error);
+      setTimeout(() => {
+        dispatch({
+          type: FILL_SONGS_LOADING,
+          payload: false,
+        });
+      }, 1000);
+      setTimeout(() => {
+        dispatch({
+          type: FILL_SONGS_ERROR,
+          payload: true,
+        });
+      }, 1000);
     }
   };
 };
-
-// export const fillSongsAction = (artistName, category) => {
-//   // this.setState({
-//   //   [category]: [...this.state[category], songInfo[0]],
-//   // });
-//   return async (dispatch, getState) => {
-//     // console.log("searchSet at fillJobsAction:", searchSet); // undefined
-//     const baseUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
-//     try {
-//       let url = baseUrl + artistName
-//       // let url =
-//       //   searchSet !== undefined ? baseUrl + "search=" + searchSet : baseUrl;
-//       let resp = await fetch(url);
-//       console.log("FETCHED:" + url);
-//       if (resp.ok) {
-//         let albums = await resp.json();
-//         let albumData = albums.data[0]
-
-//         console.log(albumData.album.title, "SONG INFO");
-
-//         setTimeout(() => {
-//           dispatch({
-//             type: FILL_SONGS_LOADING,
-//             payload: false,
-//           });
-//         }, 1000);
-//         dispatch({
-//           type: FILL_SONGS_ERROR,
-//           payload: false,
-//         });
-//         dispatch({
-//           type: FILL_SONGS,
-//           payload: {albumData, catogory},
-//           // category: {category, },
-//         });
-//       } else {
-//         console.log("error");
-//         setTimeout(() => {
-//           dispatch({
-//             type: FILL_SONGS_LOADING,
-//             payload: false,
-//           });
-//         }, 1000);
-//         setTimeout(() => {
-//           dispatch({
-//             type: FILL_SONGS_ERROR,
-//             payload: true,
-//           });
-//         }, 1000);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//       setTimeout(() => {
-//         dispatch({
-//           type: FILL_SONGS_LOADING,
-//           payload: false,
-//         });
-//       }, 1000);
-//       setTimeout(() => {
-//         dispatch({
-//           type: FILL_SONGS_ERROR,
-//           payload: true,
-//         });
-//       }, 1000);
-//     }
-//   };
-// };
