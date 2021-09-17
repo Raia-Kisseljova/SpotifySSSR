69454e591eb0a1c5688653a499f54ac29d41d2d9
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2021-09-17 14:55:02 +0100

    getting 1 artist each cat

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -71,86 +71,85 @@ class Home extends React.Component {
   };
 
   render() {
-    console.log(this.props.song.data, "THIS IS THE REDUX STATE ");
-    console.log(this.state, "THIS IS THE COMPONENT STATE");
+    console.log(this.props.song.data, "THIS IS THE REDUX STATE DATA[0]");
+    // console.log(this.state, "THIS IS THE COMPONENT STATE");
     // console.log(this.props.song, "THIS");
-    return (
-      this.props.song.data !== undefined && (
-        <Col className="col-12 col-md-9 offset-md-3 mainPage">
+    return this.props.song.error ? (
+      "ERROR!!"
+    ) : this.props.song.loading ? (
+      "LOADING!!"
+    ) : (
+      <Col className='col-12 col-md-9 offset-md-3 mainPage'>
+        <Row>
+          <div className='col-9 col-lg-11 mainLinks d-none d-md-flex'>
+            <div>TRENDING</div>
+            <div>PODCAST</div>
+            <div>MOODS AND GENRES</div>
+            <div>NEW RELEASES</div>
+            <div>DISCOVER</div>
+          </div>
+        </Row>
+        {this.props.searchResults.length > 0 && (
           <Row>
-            <div className="col-9 col-lg-11 mainLinks d-none d-md-flex">
-              <div>TRENDING</div>
-              <div>PODCAST</div>
-              <div>MOODS AND GENRES</div>
-              <div>NEW RELEASES</div>
-              <div>DISCOVER</div>
-            </div>
+            <Col xs={10}>
+              <div id='searchResults'>
+                <h2>Search Results</h2>
+                <Row className='row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3'>
+                  {this.props.searchResults.map((song) => (
+                    <AlbumCard song={song} key={song.id} />
+                  ))}
+                </Row>
+              </div>
+            </Col>
           </Row>
-          {this.props.searchResults.length > 0 && (
+        )}
+        {this.props.searchResults.length === 0 && (
+          <>
             <Row>
               <Col xs={10}>
-                <div id="searchResults">
-                  <h2>Search Results</h2>
-                  <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3">
-                    {this.props.searchResults.map((song) => (
+                <div id='rock'>
+                  <h2>Rock Classics</h2>
+                  <Row
+                    className='row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3'
+                    id='rockSection'>
+                    {this.props.song.data.rockSongs[0]?.map((song) => (
                       <AlbumCard song={song} key={song.id} />
                     ))}
                   </Row>
                 </div>
               </Col>
             </Row>
-          )}
-          {this.props.searchResults.length === 0 && (
-            <>
-              <Row>
-                <Col xs={10}>
-                  <div id="rock">
-                    <h2>Rock Classics</h2>
-                    <Row
-                      className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
-                      id="rockSection"
-                    >
-                      {this.state.rockSongs?.map((song) => (
-                        <AlbumCard song={song} key={song.id} />
-                      ))}
-                    </Row>
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={10}>
-                  <div id="pop">
-                    <h2>Pop Culture</h2>
-                    <Row
-                      className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
-                      id="popSection"
-                    >
-                      {this.state.popSongs?.map((song) => (
-                        <AlbumCard song={song} key={song.id} />
-                      ))}
-                    </Row>
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={10}>
-                  <div id="hiphop">
-                    <h2>#HipHop</h2>
-                    <Row
-                      className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
-                      id="hipHopSection"
-                    >
-                      {this.state.hipHopSongs?.map((song) => (
-                        <AlbumCard song={song} key={song.id} />
-                      ))}
-                    </Row>
-                  </div>
-                </Col>
-              </Row>
-            </>
-          )}
-        </Col>
-      )
+            <Row>
+              <Col xs={10}>
+                <div id='pop'>
+                  <h2>Pop Culture</h2>
+                  <Row
+                    className='row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3'
+                    id='popSection'>
+                    {this.props.song.data.popSongs[0]?.map((song) => (
+                      <AlbumCard song={song} key={song.id} />
+                    ))}
+                  </Row>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={10}>
+                <div id='hiphop'>
+                  <h2>#HipHop</h2>
+                  <Row
+                    className='row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3'
+                    id='hipHopSection'>
+                    {this.props.song.data.hipHopSongs[1]?.map((song) => (
+                      <AlbumCard song={song} key={song.id} />
+                    ))}
+                  </Row>
+                </div>
+              </Col>
+            </Row>
+          </>
+        )}
+      </Col>
     );
   }
 }
